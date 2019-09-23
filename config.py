a88d5ad4a74b96194793d7feb004a9052d48d6af
Ali <aliie62@yahoo.com>
2019-09-23 15:37:54 +1000

    Added PostgreSQL environment variable to config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 JWT_AUTH_URL_RULE = '/login'
 JWT_EXPIRATION_DELTA = timedelta(seconds = 600)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.environ.get(DATABASE_URL,'sqlite:///data.db')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','sqlite:///data.db')
