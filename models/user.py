afd7322e8493d9e251989764b6e7c7e32d489564
Ali Hosseini <28073162+aliie62@users.noreply.github.com>
2023-05-23 16:18:24 +1000

    Added Redis to store JWT access token

--- models/user.py
+++ models/user.py
@@ -1,17 +1,16 @@
 from config.db import db
 
+
 class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
     userGroup = db.Column(db.Integer)
     active = db.Column(db.Integer)
     locked = db.Column(db.Integer)
 
-
-
-    def __init__(self,_id,username,password, userGroup = 1, active = 1, locked = 0):
+    def __init__(self, _id, username, password, userGroup=1, active=1, locked=0):
         """
         Instance attributes:
         username
@@ -29,23 +28,20 @@ class User(db.Model):
         self.id = _id
         self.username = username
         self.password = password
+        self.userGroup = userGroup
         self.active = active
         self.locked = locked
-    
+
     def json(self):
-        return {
-            'id':self.id,
-            'usrename':self.username,
-            'active': self.active
-            }
+        return {"id": self.id, "usrename": self.username, "active": self.active}
 
     @classmethod
-    def find_by_username(cls,username):
-        return cls.query.filter_by(username=username,active=1).first()
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username, active=1).first()
 
     @classmethod
-    def find_by_id(cls,id):
-        return cls.query.filter_by(id=id,active=1).first()
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id, active=1).first()
 
     def save_to_db(self):
         db.session.add(self)
--- resources/endpoints.py
+++ resources/endpoints.py
@@ -2,18 +2,26 @@
 
 from flask_restful import Api
 from resources.item import ItemResource, ItemListResource
-from resources.user import UserResource, UserRegister, UserLogin, TokenReferesh
-from resources.store import StoreListResource,StoreResource
+from resources.user import (
+    UserResource,
+    UserRegister,
+    UserLogin,
+    UserLogout,
+    TokenReferesh,
+)
+from resources.store import StoreListResource, StoreResource
 
-#Define app end points
+
+# Define app end points
 def get_endpoints(app):
     api = Api(app)
-    api.add_resource(UserResource,'/user/<int:user_id>')
-    api.add_resource(UserLogin,'/login')
-    api.add_resource(TokenReferesh, '/refresh')
-    api.add_resource(ItemListResource,'/items')
-    api.add_resource(ItemResource,'/item','/item/<string:name>')
-    api.add_resource(UserRegister,'/register')
-    api.add_resource(StoreResource, '/store','/store/<string:name>')
-    api.add_resource(StoreListResource,'/stores')
+    api.add_resource(UserResource, "/user/<int:user_id>")
+    api.add_resource(UserLogin, "/login")
+    api.add_resource(UserLogout, "/logout")
+    api.add_resource(TokenReferesh, "/refresh")
+    api.add_resource(ItemListResource, "/items")
+    api.add_resource(ItemResource, "/item", "/item/<string:name>")
+    api.add_resource(UserRegister, "/register")
+    api.add_resource(StoreResource, "/store", "/store/<string:name>")
+    api.add_resource(StoreListResource, "/stores")
     return api
